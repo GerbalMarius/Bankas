4811b8d61e29e1f6e964d0b490a4f964896be493
Marius <mariukas.ambrazevicius@gmail.com>
2024-12-06 16:10:35 +0200

    First  commit.

--- frontend/src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-function App() {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "Labs")));
-}
-export default App;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <h1>KAZKAKDA</h1>
+            <h1>Labas</h1>
         );
     }
 }
